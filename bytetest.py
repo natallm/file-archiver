8b1b87718421a27da2d5758104989053b1f63d40
ntallm <ntnwaslost@gmail.com>
2026-04-28 20:45:03 +0700

    new upd

--- bytetest.py
+++ bytetest.py
@@ -5,17 +5,18 @@ import os
 paths_to_archive = Path("files-to-archive").rglob("**/*")
 paths_to_archive = [os.path.relpath(path) for path in paths_to_archive]
 
-with open("archive.nbz", "wb") as archive_file:
+with open("bytefile", "wb") as archive_file:
     for path_name in paths_to_archive:
         def write_to_archive(string: str):
             archive_file.write(bytes(string, "utf-8"))
-        
+
         archive_path_name = os.path.relpath(path_name, "files-to-archive")
         if os.path.isdir(path_name):
             write_to_archive("\uF674" + archive_path_name) # U+F674=directory
         else:
             write_to_archive("\uF675" + archive_path_name + "\uF676") #U+F675=file, U+F676=end of file name
-            write_to_archive(Path(path_name).read_text())
+            with open(path_name) as file:
+                write_to_archive(file.read())
 
         write_to_archive("\uF679") #U+F679=end of file
         break
